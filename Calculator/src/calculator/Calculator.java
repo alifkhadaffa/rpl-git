b0037d2e4efac8558ed07e18451dc7aad9035d55
RICHARD WIJAYA <i16014@ftis>
2018-11-02 13:46:48 +0700

    Initial Commit

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author i16014
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    public float plus(float a, float b){
+        return 0f;
+    }
+    
+    public float minus(float a, float b){
+        return 0f;
+    }
+    
+    
+    
+}
